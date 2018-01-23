2c07e0f53dcb2708a7991c5f4f9359c7442095d8
ezhang520 <ezhang520@hotmail.com>
2018-01-23 21:25:26 +0800

    no comment

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,26 @@
 print("hello world")
+import math
+
+# print(math.floor(3.14))
+# print(math.floor(3.92))
+# print(math.fabs(-3.0))
+# print(math.fmod(5, 3))
+# print(abs(3))
+# print(abs(-3.0))
+# print(round(1.234))
+# print(round(1.234, 2))
+# print(math.sqrt(9))
+# print(math.pow(9, 2))
+
+
+# test="我爱吃"
+# bytes_utf_8=test.encode(encoding="utf-8")
+# print(bytes_utf_8)
+#
+# str_from_utf_8 = bytes_utf_8.decode(encoding="utf-8")
+# print(str_from_utf_8)
+
+test = "我爱极值学院"
+bytes_utf_8 = test.encode(encoding="utf-8")
+print(bytes_utf_8)
+
